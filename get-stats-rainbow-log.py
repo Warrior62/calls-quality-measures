b4e8ca9ee34844d99602b6d3c5cfad2f8218d813
Mathis <mathistryla@gmail.com>
2021-04-29 16:01:38 +0200

    Change the log pathname typing way

--- get-stats-rainbow-log.py
+++ get-stats-rainbow-log.py
@@ -4,12 +4,7 @@
 # In[ ]:
 
 
-import re, ast, csv, sys
-
-if len(sys.argv) > 1 :
-    logname = str(sys.argv[1])
-else: 
-    sys.exit(-1)
+import re, ast, csv, sys, os
 
 mos = "MOS"
 jitter = "Jitter"
@@ -48,40 +43,42 @@ def create_stats_values_list(occ):
     return result
 
 
-if __name__ == '__main__':        
-    # Open the log file
-    with open(logname, "rt") as f:
-        content = f.read()
-        if (mos in content) and (jitter in content):
-            print("mos and jitter in content")
-            mean_mos_occ = re.findall(pattern_mean_mos, content)
-            jitter_occ = re.findall(pattern_jitter, content)
-            mos_packets_lost_audio_occ = re.findall(pattern_mos_packets_lost_audio, content)
+if __name__ == '__main__':   
+    logname = input("log pathname : ")
+    if os.path.isfile(logname):
+        # Open the log file
+        with open(logname, "rt") as f:
+            content = f.read()
+            if (mos in content) and (jitter in content):
+                print("mos and jitter in content")
+                mean_mos_occ = re.findall(pattern_mean_mos, content)
+                jitter_occ = re.findall(pattern_jitter, content)
+                mos_packets_lost_audio_occ = re.findall(pattern_mos_packets_lost_audio, content)
 
-            # Create a list which contains the meanMos values
-            result_mean_mos = create_stats_values_list(mean_mos_occ)
+                # Create a list which contains the meanMos values
+                result_mean_mos = create_stats_values_list(mean_mos_occ)
 
-            # Create a list which contains the jitter values
-            result_jitter = create_stats_values_list(jitter_occ)
+                # Create a list which contains the jitter values
+                result_jitter = create_stats_values_list(jitter_occ)
 
-            # Create a list which contains the mosPacketsLostAudio values
-            result_mos_packets_lost_audio = create_stats_values_list(mos_packets_lost_audio_occ)
+                # Create a list which contains the mosPacketsLostAudio values
+                result_mos_packets_lost_audio = create_stats_values_list(mos_packets_lost_audio_occ)
 
-            # Create a dictionnary list which contains both lists
-            for i in range(len(result_mean_mos)):
-                new_tuple = []
-                single_tuple = ("meanMOS", result_mean_mos[i])
-                new_tuple.append(single_tuple)
-                single_tuple = ("googJitterBufferMsAudio", result_jitter[i])
-                new_tuple.append(single_tuple)
-                single_tuple = ("mosPacketsLostAudio", result_mos_packets_lost_audio[i])
-                new_tuple.append(single_tuple)
-                new_tuple = dict(new_tuple)
-                stats_list.append(new_tuple)
+                # Create a dictionnary list which contains both lists
+                for i in range(len(result_mean_mos)):
+                    new_tuple = []
+                    single_tuple = ("meanMOS", result_mean_mos[i])
+                    new_tuple.append(single_tuple)
+                    single_tuple = ("googJitterBufferMsAudio", result_jitter[i])
+                    new_tuple.append(single_tuple)
+                    single_tuple = ("mosPacketsLostAudio", result_mos_packets_lost_audio[i])
+                    new_tuple.append(single_tuple)
+                    new_tuple = dict(new_tuple)
+                    stats_list.append(new_tuple)
 
-            # Create a csv file which contains stats dictionnary
-            create_csv_file(csv_file, csv_columns, stats_list)
-        else:
-            print("mos is not in content")
-        f.close()
+                # Create a csv file which contains stats dictionnary
+                create_csv_file(csv_file, csv_columns, stats_list)
+            else:
+                print("mos is not in content")
+            f.close()
 
