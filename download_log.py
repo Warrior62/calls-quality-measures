b02f035d041fae22b1993c8993f46dd9aec84849
Warrior62 <50228030+Warrior62@users.noreply.github.com>
2021-05-27 11:58:17 +0200

    Update download_log.py

--- download_log.py
+++ download_log.py
@@ -6,15 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-<<<<<<< HEAD
-
-
 mail_login = input('Rainbow mail adress: ')
 pwd_login = input('Rainbow password: ')
-=======
-mail_login = input("Rainbow mail address: ")
-pwd_login = input("Rainbow password: ")
->>>>>>> 3837048ab7f5f75e99e40fe3ce5ba9dacd3f4738
 
 rainbow_url = "https://web.openrainbow.com"
 
@@ -110,4 +103,4 @@ if __name__ == "__main__":
         download_log()
         time.sleep(3)
         driver.close()
-        print("--> PLEASE RUN get_quality_values_rainbow_log.py")
+        print("--> NOW  get_quality_values_rainbow_log.py is running...")
